62b9b80fe43be570546c3a560ac86dae0cc461a2
msanchez <martinsanchez@MacBook-Air-de-msanchez.local>
2021-05-04 12:05:26 +0200

    show nif

--- ADN/__init__.py
+++ ADN/__init__.py
@@ -24,7 +24,7 @@ class db:
         self.cursor.execute("SELECT * FROM servicetonic")
         row = self.cursor.fetchone()
         while row:
-            print(row)
+            print(row[1]+" "+row[2])
             row = self.cursor.fetchone()
 
 
